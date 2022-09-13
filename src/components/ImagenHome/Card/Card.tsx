74ae350ef4335bbd1505797f3dd296b6e898881e
Francisco Huchin <huchin.uc.francisco@hotmail.com>
2022-09-13 16:17:05 -0500

    FIXED: Optimizacion Framer Motion

--- src/components/ImagenHome/Card/Card.tsx
+++ src/components/ImagenHome/Card/Card.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { motion } from 'framer-motion' 
+import { m } from 'framer-motion' 
 
 export interface CardInterface {
   icon: string;
@@ -9,7 +9,7 @@ export interface CardInterface {
 
 const Card: React.FC<CardInterface> = ({ icon, title, className }) => {
   return (
-    <motion.div
+    <m.div
       whileInView= {{ translateX: [-50, 0, -10, 0], opacity: [ 0, .8, .9, 1] }}
       transition= {{ duration: 0.8 }}
       className={`absolute hidden md:flex items-center gap-4 p-3 rounded-xl border-2 border-shadow bg-white ${className}`}
@@ -18,7 +18,7 @@ const Card: React.FC<CardInterface> = ({ icon, title, className }) => {
         <img src={icon} alt="" />
       </div>
       <p className="pr-2 text-sm lg:pr-8">{title}</p>
-    </motion.div>
+    </m.div>
   )
 }
 
