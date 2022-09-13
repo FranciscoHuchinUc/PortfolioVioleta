20594c42f4745f6f27e3af065cd3f9878a573deb
Francisco Huchin <huchin.uc.francisco@hotmail.com>
2022-09-13 16:17:38 -0500

    2

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -9,7 +9,7 @@ const About: React.FC<AboutInterface> = () => {
 	return (
 		<Layout id='about'>
 			<h1 className='text-center text-2xl font-medium'>Sobre Mí</h1>
-			<article className='grid md:grid-cols-[400px_minmax(400px,_1fr)_0px] items-center mt-10'>
+			<article className='grid lg:grid-cols-[400px_minmax(400px,_1fr)_0px] items-center mt-10'>
 				<div className='pb-10 md:pb-0 md:pr-10'>
 					<p className='text-justify'>{data.description}</p>
 				</div>
